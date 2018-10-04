3e48fa0d048380e6b441b257e4e36feca441bf8a
Florence Hapgood <flohapgood@icloud.com>
2018-10-04 16:34:27 +0100

    **************

--- interface.js
+++ interface.js
@@ -2,17 +2,28 @@ $(document).ready(function() {
 var thermostat = new Thermostat();
 updateTemperature();
 
-var city = $('#current-city').val();
-$.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=7d7d33a65c53f4cff779a0fb09c273aa&units=metric', function(data) {
-$('#outside-temperature').text(data.main.temp)
+// $('#current-city').change(function() {
+//   var city = $('#current-city').val();
+// $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=7d7d33a65c53f4cff779a0fb09c273aa&units=metric', function(data) {
+//   $('#outside-temperature').text(data.main.temp)
+//   })
+// })
+displayWeather('London');
+
+$('#current-city').change(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  displayWeather(city);
 })
 
-$('#current-city').change(function() {
-  var city = $('#current-city').val();
-$.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=7d7d33a65c53f4cff779a0fb09c273aa&units=metric', function(data) {
-  $('#outside-temperature').text(data.main.temp)
+function displayWeather(city) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  var token = '&appid=7d7d33a65c53f4cff779a0fb09c273aa';
+  var units = '&units=metric';
+  $.get(url + token + units, function(data) {
+    $('#outside-temperature').text(data.main.temp);
   })
-})
+}
 
 $('#temperature-up').click(function() {
   thermostat.up();
